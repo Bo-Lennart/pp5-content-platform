aab93ec4f3e45461c4cbf972a125a9892eeed85d
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-10 14:36:37 +0000

    update home page logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
     <Container>
-      <Navbar.Brand href="#home">A G O Y</Navbar.Brand>
+      <Navbar.Brand href="#home">D A G O Y</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
