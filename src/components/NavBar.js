6254e47a0ec06538fc609af7cae48a5f8f1069aa
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-10 15:13:46 +0000

    move href attributes from nav links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,15 +16,15 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">H O M E</Nav.Link>
-            <Nav.Link href="#pricing">P O S T S</Nav.Link>
+            <Nav.Link>H O M E</Nav.Link>
+            <Nav.Link>P O S T S</Nav.Link>
             <NavDropdown title="C A T E G O R I E S" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">W O R L D</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">B U S I N E S S</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">F O O D</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">C U L T U R E</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">M U S I C</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">T E C H</NavDropdown.Item>
+              <NavDropdown.Item>W O R L D</NavDropdown.Item>
+              <NavDropdown.Item>B U S I N E S S</NavDropdown.Item>
+              <NavDropdown.Item>F O O D</NavDropdown.Item>
+              <NavDropdown.Item>C U L T U R E</NavDropdown.Item>
+              <NavDropdown.Item>M U S I C</NavDropdown.Item>
+              <NavDropdown.Item>T E C H</NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Nav>
