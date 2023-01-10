26c40af649be517c07efcfaa8ee7c272ae7621e1
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-10 14:31:23 +0000

    create navbar component and import to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
 import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <NavBar />
     </div>
   );
 }
