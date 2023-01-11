786f325307fa3ff2c2706d96d3e2faa2caf8c708
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-11 14:25:58 +0000

    trying to solve alert message

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,12 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-import axios from "axios";
+import axios from 'axios';
 
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
 
 const SignUpForm = () => {
     const [signUpData, setSignUpData] = useState({
@@ -15,7 +15,7 @@ const SignUpForm = () => {
         password2: '',
     })
 
-    const {username, password1, password2} = signUpData;
+    const { username, password1, password2 } = signUpData;
 
     const [errors, setErrors] = useState({});
 
@@ -27,75 +27,78 @@ const SignUpForm = () => {
         });
     };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    // sent data to api, reroute user to login page and display error message
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
         try {
-          await axios.post("/dj-rest-auth/registration/", signUpData);
-          history.push("/signin");
+            await axios.post('/dj-rest-auth/registration/', signUpData);
+            history.push('/login');
         } catch (err) {
-          setErrors(err.response?.data);
+            setErrors(err.response?.data);
         }
-      };
+    };
 
-  return (
-    <Row className={styles.Row}>
-      <Col className="my-auto py-2 p-md-2" md={6}>
-        <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign up</h1>
+    return (
+        <Row className={styles.Row}>
+            <Col className="my-auto py-2 p-md-2" md={6}>
+                <Container className={`${appStyles.Content} p-4 `}>
+                    <h1 className={styles.Header}>sign up</h1>
 
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="username">
-                    <Form.Label className="d-none">Ssername</Form.Label>
-                    <Form.Control 
-                    type="text" 
-                    placeholder="username"
-                    name="username"
-                    value={username}
-                    onChange={handleOnChange} />
-                </Form.Group>
-                {errors.username?.map((message, idx) => (
-                <Alert variant="warning" key={idx}>
-                {message}
-                </Alert>
-            ))}
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="username">
+                            <Form.Label className="d-none">Ssername</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="username"
+                                name="username"
+                                value={username}
+                                onChange={handleOnChange} />
+                        </Form.Group>
+                        {errors.username?.map((message, idx) => (
+                            <Alert variant="danger" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
 
-                <Form.Group controlId="password1">
-                    <Form.Label className="d-none">Password</Form.Label>
-                    <Form.Control 
-                    type="password" 
-                    placeholder="Password"
-                    name="password1"
-                    value={password1}
-                    onChange={handleOnChange} />
-                </Form.Group>
 
-                <Form.Group controlId="password2">
-                    <Form.Label className="d-none">Confirm password</Form.Label>
-                    <Form.Control 
-                    type="password" 
-                    placeholder="Confirm password"
-                    name="password2"
-                    value={password2}
-                    onChange={handleOnChange} />
-                </Form.Group>
+                        <Form.Group controlId="password1">
+                            <Form.Label className="d-none">Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                name="password1"
+                                value={password1}
+                                onChange={handleOnChange} />
+                        </Form.Group>
 
-                <Button variant="primary" type="submit">Sign up</Button>
-            </Form>
+                        <Form.Group controlId="password2">
+                            <Form.Label className="d-none">Confirm password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Confirm password"
+                                name="password2"
+                                value={password2}
+                                onChange={handleOnChange} />
+                        </Form.Group>
 
-        </Container>
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signin">
-            Already have an account? <span>Sign in</span>
-          </Link>
-        </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
-      >
-      </Col>
-    </Row>
-  );
+                        <Button variant="primary" type="submit">Sign up</Button>
+                    </Form>
+
+                </Container>
+                <Container className={`mt-3 ${appStyles.Content}`}>
+                    <Link className={styles.Link} to="/signin">
+                        Already have an account? <span>Sign in</span>
+                    </Link>
+                </Container>
+            </Col>
+            <Col
+                md={6}
+                className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+            >
+            </Col>
+        </Row>
+    );
 };
 
 export default SignUpForm;
