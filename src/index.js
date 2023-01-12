cd5781aaa196fdaeed5e9c45d899284edbcb46b1
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-12 09:27:59 +0000

    import userprovider into index js file

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import reportWebVitals from "./reportWebVitals";
 import {
   BrowserRouter,
 } from "react-router-dom";
+import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CurrentUserProvider>
+        <App />
+      </CurrentUserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
