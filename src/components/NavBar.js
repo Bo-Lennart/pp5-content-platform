2378aa8de527c959076829ba61269cefc80a5878
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-12 10:41:14 +0000

    tested user login and logout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,18 +12,29 @@ import {
   Link,
   Outlet,
 } from "react-router-dom";
-import { useCurrentUser } from '../contexts/CurrentUserContext';
+import { useCurrentUser, useSetCurrentUser, setCurrentUser } from '../contexts/CurrentUserContext';
 import ProfileIcon from './ProfileIcon';
+import axios from 'axios';
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post('dj-rest-auth/logout/');
+      setCurrentUser(null)
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   const addBlogPostIcon = (
     <Link to="/blogposts/create" className={styles.NavLink}><i className='far fa-plus-square'></i>P O S T</Link>
   )
   const loggedInIcons = <>
     <Link to="/bookmarks" className={styles.NavLink}><i class="fa-solid fa-book-bookmark"></i>B O O K M A R K S</Link>
-    <Link to="/" className={styles.NavLink} onClick={() => {}}><i class='fas fa-sign-out-alt'></i>Sign out</Link>
+    <Link to="/" className={styles.NavLink} onClick={handleSignOut}><i class='fas fa-sign-out-alt'></i>Sign out</Link>
 
     <Link to={`/profile/${currentUser?.profile_id}`} className={styles.NavLink} onClick={() => {}}>
       <ProfileIcon src={currentUser?.profile_image} text="Profile" height={30} /></Link>
