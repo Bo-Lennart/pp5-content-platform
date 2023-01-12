bb75fd860a4613ef8f7ec0706fa6194da144e931
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-12 09:25:18 +0000

    reafctor providers into current user context file from app js

--- src/App.js
+++ src/App.js
@@ -11,31 +11,11 @@ import {
 import './api/axiosDefault';
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
-import { createContext, useEffect, useState } from "react";
-import axios from 'axios';
-
-export const CurrentUserContext = createContext();
-export const SetCurrentUserContext = createContext();
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null)
-
-  const handleMount = async () => {
-    try {
-      const { data } = await axios.get("dj-rest-auth/user/")
-      setCurrentUser(data)
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  useEffect(() => {
-    handleMount();
-  }, []);
 
   return (
-    <CurrentUserContext.Provider value={currentUser}>
-      <SetCurrentUserContext.Provider value={setCurrentUser}>
+
         <div className={styles.App}>
           <NavBar />
           <Container className='Main'>
@@ -57,8 +37,7 @@ function App() {
             </Routes>
           </Container>
         </div>
-      </SetCurrentUserContext.Provider>
-    </CurrentUserContext.Provider>
+
   );
 }
 
--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,3 +1,29 @@
-export const CurrentUserProvider = ({children}) => {
-    
+import { createContext, useEffect, useState } from "react";
+import axios from 'axios';
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
+
+export const CurrentUserProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null)
+
+    const handleMount = async () => {
+        try {
+            const { data } = await axios.get("dj-rest-auth/user/")
+            setCurrentUser(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        handleMount();
+    }, []);
+
+    return (
+        <CurrentUserContext.Provider value={currentUser}>
+            <SetCurrentUserContext.Provider value={setCurrentUser}>
+                {children}
+            </SetCurrentUserContext.Provider>
+        </CurrentUserContext.Provider>)
 }
