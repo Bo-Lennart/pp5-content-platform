b7e009b2bc4a4f58e5ebdab44b47a8ca40f328de
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-13 09:58:13 +0000

    import post component to postpage

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import styles from "../../styles/Post.module.css";
 
-const Post = () => {
+const Post = (props) => {
   return (
-    <div>Post</div>
+    <div>Post connected</div>
   )
 }
 
