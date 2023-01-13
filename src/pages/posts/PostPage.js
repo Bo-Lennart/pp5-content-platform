0d18fdd8baad04ff82693b637263a40be87370f2
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-13 09:20:25 +0000

    render post from APi and log to console

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -6,6 +6,7 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefault";
 
 function PostPage() {
     const { id } = useParams();
@@ -14,12 +15,17 @@ function PostPage() {
     useEffect(() => {
         const handleMount = async () => {
             try {
-
-            } catch (err){
-
+                const [{ data: post }] = await Promise.all([
+                    axiosReq.get(`/blogposts/${id}`)
+                ])
+                setPost({results: [post]})
+                console.log(post)
+            } catch (err) {
+                console.log(err)
             }
         }
-    })
+        handleMount();
+    }, [id]);
 
     return (
         <Row className="h-100">
