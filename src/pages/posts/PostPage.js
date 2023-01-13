d7c68d1d6a26ac20be47bb9d924450a5a8d3e469
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-13 09:12:29 +0000

    setup basic const, use effect and handle mount code in postpage

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom";
 
 function PostPage() {
-  // Add your logic here
+    const { id } = useParams();
+    const [post, setPost] = useState({ results: [] });
 
-  return (
-    <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
-        <p>Post component</p>
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
 
-      </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular posts for desktop
-      </Col>
-    </Row>
-  );
+            } catch (err){
+
+            }
+        }
+    })
+
+    return (
+        <Row className="h-100">
+            <Col className="py-2 p-0 p-lg-2" lg={8}>
+                <p>Popular profiles for mobile</p>
+                <p>Post component</p>
+
+            </Col>
+            <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+                Popular posts for desktop
+            </Col>
+        </Row>
+    );
 }
 
 export default PostPage;
