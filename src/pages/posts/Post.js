f4542561c18b5d7e21316e9f215b1dad21d9462e
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-13 10:03:45 +0000

    set up props for post data and import to check Current user

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import styles from "../../styles/Post.module.css";
 
 const Post = (props) => {
+    const {
+        id,
+        category,
+        owner,
+        profile_id,
+        profile_image,
+        likes_count,
+        like_id,
+        title,
+        content,
+        image,
+        updated_at,
+
+    } = props;
+
+    const currentUser = useCurrentUser;
+    const is_owner = currentUser?.username === owner
+
   return (
-    <div>Post connected</div>
+    
   )
 }
 
