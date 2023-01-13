6636fc4d84cd779201ac43f397faa2a978b415a2
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-13 18:16:28 +0000

    fix results bug

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useInsertionEffect, useState } from "react";
 import Post from "./Post";
-import Form from "react-bootstrap/Form";
+
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
-
-import appStyles from "../../App.module.css";
-import styles from "../../styles/PostsPage.module.css";
+import { Container } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefault";
 
@@ -19,16 +16,17 @@ function PostsPage({ message, filter = "" }) {
     const fetchPosts = async () => {
       try {
         const { data } = await axiosReq.get(`/blogposts/?${filter}`);
-        setPosts(data);
+        setPosts({results: data});
         setHasLoaded(true);
-      } catch (error) {
-        console.log(error);
+      } catch (err) {
+        console.log(err);
       }
-    }
+    };
+
     setHasLoaded(false);
     fetchPosts();
   }, [filter, pathname]);
-
+  console.log(posts)
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
@@ -36,19 +34,23 @@ function PostsPage({ message, filter = "" }) {
         {hasLoaded ? (
           <>
             {posts.results.length ? (
-              posts.results.map(map => (
-                <Post key={Post.id} {...post} setPosts={setPosts} />
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
               ))
             ) : (
-              console.log('no results');
-          )}
+              <Container >
+                <h1>Not loaded</h1>
+              </Container>
+            )}
           </>
         ) : (
-          console.log("show loading page")
+          <Container >
+            <h1>Not loaded</h1>
+          </Container>
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular posts for desktop</p>
+        <p>Popular profiles for desktop</p>
       </Col>
     </Row>
   );
