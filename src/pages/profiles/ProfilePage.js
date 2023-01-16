dbbf25b7c1f4b23f0bf862c79b84734f41acbe83
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-16 12:23:25 +0000

    set consts for profile page

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,41 +1,57 @@
 import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from 'react-bootstrap'
+import { useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefault";
+import { CurrentUserContext, useCurrentUser } from "../../contexts/CurrentUserContext";
 import {
     useProfileData,
     useSetProfileData,
-  } from "../../contexts/ProfileDataContext";
+} from "../../contexts/ProfileDataContext";
 
 function ProfilePage() {
     const [hasLoaded, setHasLoaded] = useState(false);
-    const setProfileData = useSetProfileData();
+    const currentUser = useCurrentUser();
     const { id } = useParams();
+    const setProfileData = useSetProfileData();
+    const { pageProfile } = useProfileData();
+    const [profile] = pageProfile.results;
     const is_owner = currentUser?.username === profile?.owner;
 
-    useEffect (() => {
+    useEffect(() => {
         const fetchData = async () => {
             try {
-                
-            } catch (error) {
-                
+                const [{ data: pageProfile }] = await Promise.all([
+                    axiosReq.get(`/profiles/${id}/`),
+                ]);
+                setProfileData((prevState) => ({
+                    ...prevState,
+                    pageProfile: { results: [pageProfile] },
+                }));
+                setHasLoaded(true);
+            } catch (err) {
+                console.log(err);
             }
-        }
-    })
+        };
+        fetchData();
+    }, [id, setProfileData]);
 
-  return (
-    <Row>
-        <Col>
-        <Container>
-            {hasLoaded ? (
-                <>
-                
-                </>
-            ) : (
-                <h1>Cannot find content</h1>
-            )}
-        </Container>
-        </Col>
-    </Row>
-  )
+
+
+    return (
+        <Row>
+            <Col>
+                <Container>
+                    {hasLoaded ? (
+                        <>
+
+                        </>
+                    ) : (
+                        <h1>Cannot find content</h1>
+                    )}
+                </Container>
+            </Col>
+        </Row>
+    )
 }
 
 export default ProfilePage
