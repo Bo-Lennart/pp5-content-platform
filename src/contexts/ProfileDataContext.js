498f2893b98a64013ea668084a6a62090e986d49
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-16 12:01:38 +0000

    add create context for profile datacontext

---
+++ src/contexts/ProfileDataContext.js
@@ -0,0 +1,9 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
+
+const ProfileDataContext = createContext();
+const SetProfileDataContext = createContext();
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
