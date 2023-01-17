016560985b6501258bf4060718bf189fb79612af
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-17 09:31:09 +0000

    render posts for current logged in profile

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -3,9 +3,15 @@ import { Link, useParams } from 'react-router-dom';
 import { axiosReq } from '../../api/axiosDefault';
 import ProfileIcon from '../../components/ProfileIcon';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { Container } from "react-bootstrap";
+import Post from '../posts/Post';
 
 
-function Profile(filter = "") {
+function Profile({filter = ""}) {
+    const [posts, setPosts] = useState({ results: [] });
+    const [hasLoaded, setHasLoaded] = useState(false);
     const currentUser = useCurrentUser();
     //get logged in user
     const currentOwner = currentUser.username;
@@ -25,6 +31,11 @@ function Profile(filter = "") {
                 // console.log(profiles.data)
                 setProfiles({ results: userData.data });
 
+                const url = `/blogposts/`
+                const { data } = await axiosReq.get(url);
+                setPosts({ results: data });
+                setHasLoaded(true);
+
             } catch (err) {
                 console.log(err)
             }
@@ -34,9 +45,29 @@ function Profile(filter = "") {
     console.log("USER INLOGGED NOW", currentOwner)
     console.log("USER DATA NOW", userData)
     return (
-        <div>
-            <h1>HELLO</h1>
-        </div>
+        <Row className="h-100">
+            <Col className="py-2 p-0 p-lg-2" lg={8}>
+                {hasLoaded ? (
+                    <>
+                        {posts.results.length ? (
+                            posts.results
+                                .filter(post => filter.length === 0 || post.owner.toLocaleLowerCase() === currentOwner.toLocaleLowerCase())
+                                .map((post) => (
+                                    <Post key={post.id} {...post} setPosts={setPosts} isInPostPage={false} />
+                                ))
+                        ) : (
+                            <Container >
+                                <h1>Not posts found</h1>
+                            </Container>
+                        )}
+                    </>
+                ) : (
+                    <Container >
+                        <h1>Loading...</h1>
+                    </Container>
+                )}
+            </Col>
+        </Row>
     )
 }
 
