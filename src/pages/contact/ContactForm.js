2905dcf09da89ef9b345e83ea6a123b1080e7141
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-17 13:48:22 +0000

    copy-paste handle submit from createPost form

--- src/pages/contact/ContactForm.js
+++ src/pages/contact/ContactForm.js
@@ -1,6 +1,28 @@
 import React from 'react'
 
 export default function ContactForm() {
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const formData = new FormData();
+    
+        formData.append('title', title)
+        formData.append('content', content)
+        formData.append('image', imageInput.current.files[0])
+        formData.append('category', category)
+    
+        try {
+          const { data } = await axiosReq.post('/blogposts/', formData);
+          navigate(`/posts/${data.id}`)
+        } catch (err) {
+          console.log(err)
+          if (err.response?.status !== 401) {
+            setErrors(err.response?.data);
+          }
+        }
+      }
+
+
   return (
     <h1>Helloooo Contact form</h1>
   )
