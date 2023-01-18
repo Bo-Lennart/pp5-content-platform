21a66b163375a3c64a4307ef17e5c7e9ea3a080f
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-18 11:24:39 +0000

    copy paste profile edit form

--- src/pages/profiles/UserPasswordForm.js
+++ src/pages/profiles/UserPasswordForm.js
@@ -1,9 +1,183 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
-const UserPasswordForm = () => {
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Upload from "../../assets/upload.png"
+
+import styles from "../../styles/PostCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import Asset from "../../components/Asset";
+import { Image } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefault";
+import { useNavigate } from "react-router-dom";
+import { Alert } from "react-bootstrap";
+
+
+
+function UserPasswordForm() {
+  const [errors, setErrors] = useState({});
+
+  const [postData, setPostData] = useState({
+    title: '',
+    content: '',
+    image: '',
+    category: '',
+  });
+  const { title, content, image, category } = postData;
+
+  const imageInput = useRef();
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setPostData({
+      ...postData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setPostData({
+        ...postData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
+  console.log(postData)
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const formData = new FormData();
+
+    formData.append('title', title)
+    formData.append('content', content)
+    formData.append('image', imageInput.current.files[0])
+    formData.append('category', category)
+
+    try {
+      const { data } = await axiosReq.post('/blogposts/', formData);
+      navigate(`/posts/${data.id}`)
+    } catch (err) {
+      console.log(err)
+      if (err.response?.status !== 401) {
+        setErrors(err.response?.data);
+      }
+    }
+  }
+
+  const textFields = (
+    <div className="text-center">
+      <Form.Group controlId="formFile">
+        <Form.Label>Title</Form.Label>
+        <Form.Control type="text" name="title" value={title} onChange={handleChange} />
+      </Form.Group>
+      {errors?.title?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+      <Form.Group controlId="formFile">
+        <Form.Label>Content</Form.Label>
+        <Form.Control as="textarea" rows={6} name="content" value={content} onChange={handleChange} />
+      </Form.Group>
+      {errors?.content?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+
+      <Form.Group>
+        <Form.Label>Category</Form.Label>
+        <Form.Control
+          as="select"
+          defaultValue="Choose category..."
+          name="category"
+          onChange={handleChange}
+          aria-label="category choice"
+        >
+          <option value="WORLD">WORLD</option>
+          <option value="BUSINESS">BUSINESS</option>
+          <option value="FOOD">FOOD</option>
+          <option value="CULTURE">CULTURE</option>
+          <option value="MUSIC">MUSIC</option>
+          <option value="TECH">TECH</option>
+        </Form.Control>
+      </Form.Group>
+      {errors?.category?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+
+      <Button className={``} onClick={() => { }}>cancel</Button>
+      <Button className={``} type="submit">create</Button>
+    </div>
+  );
+
   return (
-    <div>UserPasswordForm</div>
-  )
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}>
+            <Form.Group className="text-center">
+
+              {image ? (
+                <>
+                  <figure>
+                    <Image className={appStyles.Image} src={image} rounded></Image>
+                  </figure>
+
+                  <div>
+                    <Form.Label type="file" htmlFor="image-upload" onClick={handleChangeImage}>
+                      <p>Change image:</p>
+                    </Form.Label>
+                  </div>
+                </>
+              ) : (
+                <Form.Label
+                  className="d-flex justify-content-center"
+                  htmlFor="image-upload"
+                >
+                  <Asset
+                    src={Upload}
+                    message="Click or tap to upload an image"
+                  />
+                </Form.Label>
+              )}
+
+              <Form.Control
+                type="file"
+                id="image-upload"
+                accept="image/*"
+                onChange={handleChangeImage}
+                ref={imageInput}
+              />
+
+            </Form.Group>
+            {errors?.image?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <div className="d-md-none">{textFields}</div>
+          </Container>
+        </Col>
+        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+          <Container className={appStyles.Content}>{textFields}</Container>
+        </Col>
+      </Row>
+    </Form >
+  );
 }
 
-export default UserPasswordForm
+export default UserPasswordForm;
